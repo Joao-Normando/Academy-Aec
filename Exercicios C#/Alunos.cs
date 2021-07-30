4eb8da8d378f31334bbb1e0c40935bb9cc310dba
Joao_Normando <joao.normando@maisunifacisa.com.br>
2021-07-30 16:47:22 -0300

    Project created

---
+++ Exercicios C#/Alunos.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaNetframework
+{
+    class Alunos
+    {
+        private string Nome;
+        public string nome
+        {
+       
+
+            get
+            {
+                return this.Nome ;
+            }
+            set
+            {
+                this.Nome = value;
+            }
+
+        }
+        private double Nota1;
+        public double nota1
+        {
+            get
+            {
+                return this.Nota1;
+            }
+            set
+            {
+                this.Nota1 = value;
+            }
+        }
+
+        private double Nota2;
+        public double nota2
+        {
+            get
+            {
+                return this.Nota2;
+            }
+            set
+            {
+                this.Nota2 = value;
+            }
+        }
+        private double Nota3;
+        public double nota3
+        {
+            get
+            {
+                return this.Nota3;
+            }
+            set
+            {
+                this.Nota3 = value;
+            }
+        }
+        public void calculaMedia()
+        {
+            Media = (this.Nota1 + this.Nota2 + this.Nota3) / 3;
+            Media = Math.Round(media, 2);
+
+        }
+
+        private double Media;
+        public double media
+        {
+
+            get
+            {
+                return this.Media;
+            }
+            set
+            {
+                this.Media = value;
+            }
+        }
+
+
+
+
+
+
+        public void mediaNotas()
+        {
+            Console.WriteLine("\nNome : " + this.Nome);
+            Console.WriteLine("Nota 1 : " + this.Nota1);
+            Console.WriteLine("Nota 2 : " + this.Nota2);
+            Console.WriteLine("Nota 3 : " + this.Nota3);
+            Console.WriteLine("Média das três notas : " + this.Media + "\n");
+        }
+
+    }
+}
+
---
+++ Exercicios C#/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaNetframework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var alunos = new List<Alunos>();
+            int contador = 0;
+            while (contador <= 3)
+
+
+            {
+
+                Alunos obj = new Alunos();
+                String entrada = " ";
+                Console.WriteLine("Calcula a média do aluno");
+                Console.Write("Aluno: ");
+                entrada = Console.ReadLine();
+                obj.nome = entrada;
+                Console.Write("Nota 1 : ");
+                entrada = Console.ReadLine();
+                obj.nota1 = Convert.ToDouble(entrada);
+                Console.Write("Nota 2 : ");
+                entrada = Console.ReadLine();
+                obj.nota2 = Convert.ToDouble(entrada);
+                Console.Write("Nota 3 : ");
+                entrada = Console.ReadLine();
+                obj.nota3 = Convert.ToDouble(entrada);
+
+                obj.calculaMedia();
+
+                obj.mediaNotas();
+                alunos.Add(obj);
+                contador++;
+            }
+
+            Console.WriteLine("\n -------------------------------------------\n");
+            foreach (var aluno in alunos) Console.WriteLine( "\n Nome: " +aluno.nome + " | Media: " + aluno.media);
+
+        }
+    }
+}
