37738c10167ecc38a8b0cbec3016bea78c89b7a0
Joao_Normando <joao.normando@maisunifacisa.com.br>
2021-07-31 12:42:47 -0300

    Praticando a tabuada em C# , estruturas de repeticao

---
+++ Tabuada/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estruturaRepeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            Console.WriteLine("Informe o numero para tabuada : ");
+                int numero = Convert.ToInt32(Console.ReadLine());
+
+                for (int x= 1; x <= 10; x++)
+                {
+                    Console.WriteLine(numero + " x " + x+ " = " + numero * x);
+            }
+                contador++;
+
+            for (float x = 1; x <= 10; x++)
+            {
+                Console.WriteLine("Divisão : "+numero + " / " + x + " = " + numero / x);
+                
+            }
+    
+            for (int x = 1; x <= 10; x++)
+            {
+                Console.WriteLine("Soma : " + numero + " + " + x + " = " + numero + x);
+            }
+            Console.ReadLine();
+            }
+        }
+    }
